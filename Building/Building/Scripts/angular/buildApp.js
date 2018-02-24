8f7117fdf8e82a82569c32323b9291deff5cb05a
MaryaSemyakova <s.mashustik@mail.ru>
2018-02-24 13:52:07 +0300

    Верстка блока услуг #7

--- Building/Building/Scripts/angular/buildApp.js
+++ Building/Building/Scripts/angular/buildApp.js
@@ -1,1 +1,13 @@
-﻿var buildApp = angular.module("buildApp", [])
+﻿var buildApp = angular.module("buildApp", []).directive("serviceBlock", function () {
+   
+    return {
+        scope: {
+            blockhead: '@',
+            blockdescription: '@',
+            imageurl: '@',
+        },
+        restrict: "E",
+        templateUrl: "/Scripts/angular/templates/serviceBlockTemplate.html",
+        
+    }
+});
--- Building/Building/Scripts/angular/mainController.js
+++ Building/Building/Scripts/angular/mainController.js
@@ -1,6 +1,42 @@
 ﻿buildApp.controller("mainController", ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
     $scope.emailFormat = /^[a-z]+[a-z0-9._]+@[a-z]+\.[a-z.]{2,5}$/;
-
+    $scope.services = {
+        Work: {
+            blockHead: 'ремонт офисов',
+            blockDescription: 'Обслуживание торговых центров, оформление фасадов',
+            imageUrl: '/Content/images/work.png'
+        },
+        Home: {
+            blockHead: 'ремонт квартир',
+            blockDescription: 'Обслуживание торговых центров, оформление фасадовКосметический и капитальные ремонт квартир , от дизайн проекта до сборки мебели',
+            imageUrl: '/Content/images/home.png'
+        },
+        Build: {
+            blockHead: 'строительство',
+            blockDescription: 'Строительство домов из кирпича, пеноблока под ключ. Проведение внутренних и наружных оттделочных работ',
+            imageUrl: '/Content/images/build.png'
+        },
+        Tubing: {
+            blockHead: 'отопление, канализация, водопровод',
+            blockDescription: 'Грамотный расчет, своевременная поставка материала и оборудования, качественный и профессиональный монтаж',
+            imageUrl: '/Content/images/Трубы.png'
+        },
+        Electricity: {
+            blockHead: 'электромонтажные работы',
+            blockDescription: 'Обслуживание торговых центров, оформление фасадов. Разработка проекта электроснабжения, монтаж и замена электропроводки, монтаж и сборка электрощитов, монтаж приборов, выдача техотчетов',
+            imageUrl: '/Content/images/electricity.png'
+        },
+        Window: {
+            blockHead: 'остекление',
+            blockDescription: 'Пластиковые окна, балконное остекление любой сложности',
+            imageUrl: '/Content/images/window.png'
+        },
+        Cleaning: {
+            blockHead: 'уборка',
+            blockDescription: 'Уборка помещений, квартир, офисов, генеральная уборка, в том числе после ремонта, мойка окон',
+            imageUrl: '/Content/images/cleaning.png'
+        },
+    }
     $scope.sendRequestPrice = function (form, request) {
         $scope.sendRequestCommon(form, request, '/Home/SendRequestPrice', "#requestPrice");
     }
@@ -14,7 +50,7 @@
             $http({
                 url: url,
                 method: "POST",
-                data: { 'request': request}
+                data: { 'request': request }
             }).
                 then(function (response) {
                     $scope.status = response.status;
